02e2eb0015db2ab79254e3d9d681ba473dddb1f0
RaviTejaKaranam <ravitejakaranam1999@gmail.com>
2021-06-08 07:30:34 +0530

    Updated .env

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const fildUploadRouter = require("./routes/fileUploads");
 app.use(morgan("dev"));
 app.use(cors());
 app.use(bodyParser.json());
-console.log(dotenv.config());
+console.log(dotenv.config({silent : true}));
 
 //Connect to the database
 
